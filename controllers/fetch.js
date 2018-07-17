f8e117e93fbda061760c5d1a3f051843b2688ecc
matt babcock <babcockmatthew12@yahoo.com>
2018-07-17 04:08:11 -0700

    update_1

---
+++ controllers/fetch.js
@@ -0,0 +1,16 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var url = "http://www.foxnews.com/"
+request('http://www.foxnews.com/', function (error, response, html){
+    var $ = cheerio.load(html);
+    var headline = $('.kicker-text');
+    var headlineText = headline.text();
+    var realtedItem = $('.related-item')
+    var realtedItemText= realtedItem.text();
+   // var linkStuff = $('.m')
+   // var linkStuffText= linkStuff.text();
+   // I need to create code that can pull in an href for the link
+
+
+    console.log(headlineText,realtedItemText)
+})
